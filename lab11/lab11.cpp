732675ce1c7bed14599b5aa630a5bfd73614a505
Alex <aortega014@student.butte.edu>
2017-08-24 04:37:24 +0000

    Lab1.1 submission

---
+++ lab11/lab11.cpp
@@ -0,0 +1,14 @@
+Have computer select a number between 0-19.
+Randomized number is selected by computer.
+Only computer knows the randomized number.
+User will need to select a program capable of attempting to guess the number chosen by the computer.
+Once the program is selected, The user will implement the parameters 0-19 for the program to guess between.
+The user will start off with a total point tally of 10.
+In the event of the program guessing correctly when run the first time, the user wins.
+However, if the program is incorrect the computer should notify the program whether to guess higher or lower.
+The user will also lose 1 point from their tally of 10 for each incorrect number guessed by the program.
+The user will repeatedly run the program until the correct answer is provided.
+Once the correct answer is provided, the user "wins"
+However, if the user runs out of points (goes from 10-0) they "lose".
+Run the program with the input parameters.
+Repeat step 13 (while following the point rules) until the user either "wins" or "loses".
