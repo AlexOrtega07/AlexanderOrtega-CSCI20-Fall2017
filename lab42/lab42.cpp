b8a825ef2a278f583c0aadec9517b207a607cc4e
Alex <aortega014@student.butte.edu>
2017-11-27 06:41:51 +0000

    Lab 4.2 submission

--- lab42/lab42.cpp
+++ lab42/lab42.cpp
@@ -38,3 +38,57 @@
      cout << endl << "In your cart there are a total of  " << totalquantity << " items for a total of $"// complete toatal
             << totalprice << "." << endl;
  }
+ /*Enter how many of this item you'd like, Bones: 5
+
+You ordered 5 Bones for a total of $20.
+Your current total is $20
+
+Enter how many of this item you'd like, Chew: 3
+
+You ordered 3 Chew for a total of $12.
+Your current total is $32
+
+Enter how many of this item you'd like, Pig Ear: 50
+There are 5 in stock, they will be added to your order.
+
+You ordered 5 Pig Ear for a total of $20.
+Your current total is $52
+
+Enter how many of this item you'd like, Larynx: 3
+
+You ordered 3 Larynx for a total of $12.
+Your current total is $64
+
+Enter how many of this item you'd like, Crunchy: 4
+
+You ordered 4 Crunchy for a total of $16.
+Your current total is $80
+
+Enter how many of this item you'd like, Soft: 5
+
+You ordered 5 Soft for a total of $20.
+Your current total is $100
+
+Enter how many of this item you'd like, Dental Chew: 2
+
+You ordered 2 Dental Chew for a total of $4.
+Your current total is $104
+
+Enter how many of this item you'd like, Animal Hoove: 6
+
+You ordered 6 Animal Hoove for a total of $18.
+Your current total is $122
+
+Enter how many of this item you'd like, Rawhide: 7
+There are 3 in stock, they will be added to your order.
+
+You ordered 3 Rawhide for a total of $15.
+Your current total is $137
+
+Enter how many of this item you'd like, Jerky Treats: 5
+
+You ordered 5 Jerky Treats for a total of $50.
+Your current total is $187
+
+In your cart there are a total of  41 items for a total of $187.
+*/
